04e9149a7e4bfbdf70ebfcea4ad3dc17ab491366
Marcos Kuchak <mkuchak@hotmail.com>
2024-02-10 16:05:39 -0300

    feat: implement automatic slug generation using slugify library

--- src/features/post/hooks/use-get-post.tsx
+++ src/features/post/hooks/use-get-post.tsx
@@ -1,10 +1,13 @@
-import { getPosts,PostMetadata } from "@/features/post/api/get-posts";
+import slugify from "slugify";
+
+import { getPosts, PostMetadata } from "@/features/post/api/get-posts";
 import { parseMarkdown } from "@/lib/parse-markdown";
 
 export async function useGetPost(slug: string) {
   const rawPosts = await getPosts();
   const posts = rawPosts.map((post) => {
     const { data, content } = parseMarkdown<PostMetadata>(post.body || "");
+    data.slug = slugify(data.title, { lower: true, strict: true });
 
     return { data, content, ...post };
   });
