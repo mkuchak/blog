eb6075150ad58d014fbbdeb63d7fc1f748530242
Marcos Kuchak <mkuchak@hotmail.com>
2024-02-11 20:27:06 -0300

    fix: correct alignment of columns in markdown tables

--- src/features/post/components/table-mdx.tsx
+++ src/features/post/components/table-mdx.tsx
@@ -5,7 +5,9 @@ import { cn } from "@/lib/utils";
 export function TableMDX({ className, ...props }: ComponentProps<"table">) {
   return (
     <div className="grid">
-      <table className={cn("grid overflow-x-auto", className)} {...props} />
+      <div className="grid overflow-x-auto">
+        <table className={cn("my-0", className)} {...props} />
+      </div>
     </div>
   );
 }
