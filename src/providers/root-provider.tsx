51e43f6a1f3140c58b04c04c416dbfa72ff66d00
Marcos Kuchak <mkuchak@hotmail.com>
2024-02-26 15:43:08 -0300

    feat: add progress bar for feedback on page transitions when link is clicked

--- src/providers/root-provider.tsx
+++ src/providers/root-provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import NextTopLoader from "nextjs-toploader";
 import { PropsWithChildren } from "react";
 
 import { ThemeProvider } from "@/components/theme/theme-provider";
@@ -7,6 +8,7 @@ import { ThemeProvider } from "@/components/theme/theme-provider";
 export function RootProvider({ children }: PropsWithChildren) {
   return (
     <>
+      <NextTopLoader color="#fdbc16" height={3} showSpinner={false} />
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
